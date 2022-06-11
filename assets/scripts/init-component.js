5a8975c4d2b4e3401a2060c6b71f36665dfb02f7
Михаил <m.prugov@mail.ru>
2022-06-11 13:17:22 +0300

    da da da

---
+++ assets/scripts/init-component.js
@@ -0,0 +1,65 @@
+'use-strict'
+
+/* Стили */
+const layoutStyles = {
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    position: 'fixed',
+    left: '0',
+    top: '0',
+
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
+
+const styles = {
+    backgroundColor: '#fefefe',
+    padding: '0.5rem',
+
+    minWidth: '280px',
+    maxWidth: '600px',
+    width: '50%',
+    height: '50%',
+
+    textAlign: 'center',
+    color: '#222222',
+
+    borderRadius: '0.5rem',
+}
+
+/* Если найден блок с переданным id, то отдаём его, иначе создаём новый */
+function getElement(id) {
+    let cont = document.getElementById(id)
+    if (cont) return cont
+    cont = document.createElement('div')
+    cont.id = 'my_modal'
+    document.body.prepend(cont)
+    return cont
+}
+
+/* Вспомогательная функция для создания компонента */
+window.createReactComponent = function (id = '', param = {}) {
+    const Modal = () => {
+        const [display, setDisplay] = React.useState(true)
+        return (
+            <>
+                {display && (
+                    <div style={layoutStyles} onClick={() => setDisplay(false)}>
+                        <div style={styles}>
+                            <h3>Params</h3>
+                            <pre>
+                                <code>{JSON.stringify(param, 2)}</code>
+                            </pre>
+                        </div>
+                    </div>
+                )}
+            </>
+        )
+    }
+
+    const container = getElement(id)
+    const root = ReactDOM.createRoot(container)
+    root.render(<Modal />)
+}
